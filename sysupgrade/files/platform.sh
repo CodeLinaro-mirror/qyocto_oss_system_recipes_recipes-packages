c697e77425372add1e661867b7b816401b58ba9a
Vijay Balaji <quic_vijbal@quicinc.com>
2023-01-20 11:11:34 +0530

    sysupgrade : Added Wifi FW support for qcn9224 targets
    This change adds wifi fw name in get_fw function to
    flash the wifi fw in qcn9224 target.
    
    Change-Id: I0ed2b2251154f00de98f38d953d1988445fb2ef9
    Signed-off-by: Vijay Balaji <quic_vijbal@quicinc.com>

--- sysupgrade/files/platform.sh
+++ sysupgrade/files/platform.sh
@@ -341,6 +341,7 @@ get_fw_name() {
 	cat /proc/device-tree/model | grep -q 9574 && img="ipq9574"
 
 	wifi_ipq="ignored"
+	image_suffix="qcn9000_qcn9224_v2_dualmac"
 	if lsmod | grep ath1 > /dev/null 2>&1 ; then
 		image_suffix="qcn9224_v2"
 	fi
